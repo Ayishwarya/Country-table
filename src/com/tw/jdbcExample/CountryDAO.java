94c24ddb6e97d70da61904d227a809dbc3d42d4b
Ayishwarya <ayishu.strawberry@gmail.com>
2017-03-06 15:39:35 +0530

    <Ayishwarya> | Web page creation

---
+++ src/com/tw/jdbcExample/CountryDAO.java
@@ -0,0 +1,11 @@
+package com.tw.jdbcExample;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CountryDAO {
+    List<Country> getAllCountries() throws SQLException;
+    Country getCountryByName(String getCountryWithName) throws SQLException;
+    boolean insertCountry(Country country)throws SQLException;
+    boolean updateCountry(Country country)throws SQLException;
+}
